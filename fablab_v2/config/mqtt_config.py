9ace7e2bd90401b67c12ca59af5faf8fb7a0a125
Nukap131 <nukaaqqalu@gmail.com>
2026-03-16 11:13:30 +0100

    Update mqtt_config.py

--- fablab_v2/config/mqtt_config.py
+++ fablab_v2/config/mqtt_config.py
@@ -1,1 +1,9 @@
-code here
+# MQTT broker
+MQTT_BROKER = "localhost"
+MQTT_PORT = 1883
+
+# Topics
+TOPIC_PERSON_IN = "fablab/people/in"
+TOPIC_PERSON_OUT = "fablab/people/out"
+TOPIC_TOTAL = "fablab/people/total"
+TOPIC_INSIDE = "fablab/people/inside"
